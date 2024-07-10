c81ae0f9d933e30ef6a40f7728bd8a38e3b12206
Wren <OtherwiseJunk@gmail.com>
2024-07-10 05:30:58 +0000

    add additional tests

--- backend/chatService.spec.ts
+++ backend/chatService.spec.ts
@@ -1,57 +1,184 @@
 import { afterEach, beforeAll, describe, expect, it, vi } from "vitest";
 import { ChatService } from './chatService';
 import { Server } from "socket.io";
-import { Metadata, Metascraper } from "metascraper";
+import { Metadata } from "metascraper";
 import got from "got";
 import { ChatData } from "./types/socketTypes";
+import { lobbyConfig, chatConfig } from "./config/configurationConstants";
+import * as discordMarkdown from "discord-markdown";
+import * as markdownToTextEmoji from "markdown-to-text-emoji";
 
-const ioMock = vi.mock('socket.io', () =>{
-    
-});
-const metascraperMock = vi.mock('metascraper');
-const gotMock = vi.mock('got');
+const existingStreamKey = 'existingStreamKey';
+const zeroUserStreamKey = 'zeroUserStreamKey';
+const messageDuration = 100;
 
-describe('ChatService', () =>{
-    let sut: ChatService;    
+function buildInitialUserCountMap() {
+    return new Map([[existingStreamKey, 1], [zeroUserStreamKey, 0]]);
+}
 
-    beforeAll(() =>{
+function buildInitialMessageMap() {
+    return new Map([[existingStreamKey, [{ message: 'test message' }]]]);
+}
 
+describe('ChatService', () => {
+    let sut: ChatService;
+    let io: Server;
+    let metascraperMock = vi.fn();
+
+    beforeAll(() => {
+        io = new Server();
+        sut = new ChatService(
+            io,
+            buildInitialUserCountMap(),
+            buildInitialMessageMap(),
+            metascraperMock,
+            got,
+            messageDuration);
     });
 
-    describe('setEmbed', () =>{
+    describe('setEmbed', () => {
         let messageData: ChatData;
-        
+
         it('should set the embed data if a URL is found in the message', async () => {
             messageData = { message: 'Check out this website: https://example.com' };
-            const embedData: Metadata = { title: 'Example Domain', url: 'https://example.com', image: 'https://cdn.com/someCoolPng.png' };
-            const mockMetadata = ioMock.fn().mockImplementation(metascraper).mockResolvedValue(embedData);
-    
-            let embedData = await sut.buildEmbed(messageData);
-    
-            expect(messageData.embed).toEqual(embedData);
-            expect(mockMetadata).toHaveBeenCalledOnce();
+            const expectedEmbed: Metadata = { title: 'Example Domain', url: 'https://example.com', image: 'https://cdn.com/someCoolPng.png' };
+            metascraperMock.mockResolvedValue(expectedEmbed);
+
+            let embed = await sut.buildEmbed(messageData);
+
+            expect(embed).toEqual(expectedEmbed);
+            expect(metascraperMock).toHaveBeenCalledOnce();
         });
-    
+
         it('should not set the embed data if no URL is found in the message', async () => {
             messageData = { message: 'This is a message without a URL' };
-    
-            await setEmbed(messageData);
-    
-            expect(messageData.embed).toBeUndefined();
+
+            let embed = await sut.buildEmbed(messageData);
+
+            expect(embed).toBeUndefined();
         });
-    
+
         it('should handle errors when fetching the URL', async () => {
             messageData = { message: 'Check out this website: https://example.com' };
-            const mockMetadata = vi.fn().mockImplementation(metascraper).mockRejectedValue(new Error('Network error'));
-    
-            await setEmbed(messageData, mockMetadata);
-    
-            expect(messageData.embed).toBeUndefined();
-            expect(mockMetadata).toHaveBeenCalledOnce();
+            metascraperMock.mockRejectedValue(new Error('Network error'));
+
+            let embed = await sut.buildEmbed(messageData);
+
+            expect(embed).toBeUndefined();
+            expect(metascraperMock).toHaveBeenCalled();
         });
     })
 
-    afterEach(() =>{
-        vi.restoreAllMocks();
-    })
-})
+    describe('incrementUserCount', () => {
+        it('should increment the user count if the stream key is not in the map', () => {
+            const testKey = 'nonExistentStreamKeyIncrement';
+
+            sut.incrementUserCount(testKey);
+            const afterCount = sut.userCountsByStreamKey.get(testKey);
+
+            expect(afterCount).toBe(1);
+        });
+        it('should increment the user count if the stream key is in the map', () => {
+            sut.incrementUserCount(existingStreamKey);
+            const afterCount = sut.userCountsByStreamKey.get(existingStreamKey);
+
+            expect(afterCount).toBe(2);
+        })
+    });
+
+    describe('decrementUserCount', () => {
+        it('should not decrement the user count if the stream key is not in the map', () => {
+            const testKey = 'nonExistentStreamKeyDecrement';
+
+            sut.decrementUserCount(testKey);
+            const afterCount = sut.userCountsByStreamKey.get(testKey);
+
+            expect(afterCount).toBe(undefined);
+        });
+        it('should decrement the user count if the stream key is in the map', () => {
+            sut.decrementUserCount(existingStreamKey);
+            const afterCount = sut.userCountsByStreamKey.get(existingStreamKey);
+
+            expect(afterCount).toBe(0);
+        })
+        it('should not decrement the user count to a negative number if the stream key is in the map', () => {
+            sut.decrementUserCount(zeroUserStreamKey);
+            const afterCount = sut.userCountsByStreamKey.get(zeroUserStreamKey);
+
+            expect(afterCount).toBe(0);
+        })
+    });
+
+    describe('getConfiguration', () => {
+        it('should return lobbyConfig for "lobby"', () => {
+            const result = sut.getConfiguration('lobby');
+            expect(result).toEqual(lobbyConfig);
+        });
+
+        it('should return chatConfig for "chat"', () => {
+            const result = sut.getConfiguration('chat');
+            expect(result).toEqual(chatConfig);
+        });
+
+        it('should return undefined for unknown configuration types', () => {
+            const result = sut.getConfiguration('unknown');
+            expect(result).toBeUndefined();
+        });
+    });
+
+    describe('handleChatMessage', () => {
+        it('should build and emit a chat message', async () => {
+            const streamKey = 'testStreamKey';
+            const messageData = { message: '**test** _message_' };
+            const toSpy = vi.spyOn(io, 'to');
+            const buildEmbedSpy = vi.spyOn(sut, 'buildEmbed').mockResolvedValueOnce(undefined);
+            const textEmojiSpy = vi.spyOn(markdownToTextEmoji, 'textEmoji').mockReturnValue(messageData.message);
+
+            await sut.handleChatMessage(streamKey, messageData);
+            const messagesAfter = sut.messagesByStreamKey.get(streamKey)!;
+
+            expect(buildEmbedSpy).toHaveBeenCalledWith(messageData);
+            expect(toSpy).toHaveBeenCalledWith(streamKey);
+            expect(textEmojiSpy).toHaveBeenCalledWith(messageData.message);
+            expect(messagesAfter.length).toBe(1);
+            expect(messagesAfter[0]).toEqual({ ...messageData, html: '<strong>test</strong> <em>message</em>', embed: undefined });
+        });
+
+        it('should build and emit a chat message with an embed', async () => {
+            const streamKey = 'testStreamKey';
+            const messageData = { message: '**test** _message_' };
+            const toSpy = vi.spyOn(io, 'to');
+            const buildEmbedSpy = vi.spyOn(sut, 'buildEmbed').mockResolvedValueOnce({ description: 'test description' });
+            const textEmojiSpy = vi.spyOn(markdownToTextEmoji, 'textEmoji').mockReturnValue(messageData.message);
+
+            await sut.handleChatMessage(streamKey, messageData);
+            const messagesAfter = sut.messagesByStreamKey.get(streamKey)!;
+
+            expect(buildEmbedSpy).toHaveBeenCalledWith(messageData);
+            expect(toSpy).toHaveBeenCalledWith(streamKey);
+            expect(textEmojiSpy).toHaveBeenCalledWith(messageData.message);
+            expect(sut.messagesByStreamKey.get(streamKey)!.length).toBe(1);
+            expect(messagesAfter.length).toBe(1);
+            expect(messagesAfter[0]).toEqual({ ...messageData, html: '<strong>test</strong> <em>message</em>', embed: { description: 'test description' } });
+        });
+
+        it('should remove the chat message after configured messageDuration', async () => {
+            const streamKey = 'testStreamKey';
+            const messageData = { message: 'test message' };
+            const removeSpy = vi.spyOn(Array.prototype, 'splice');
+
+            await sut.handleChatMessage(streamKey, messageData);
+
+            // sleep for 5 minutes
+            await new Promise(resolve => setTimeout(resolve, messageDuration));
+            expect(removeSpy).toHaveBeenCalledWith(0, 1)
+            expect(sut.messagesByStreamKey.get(streamKey)!.length).toBe(0);
+        });
+
+        afterEach(() => {
+            io.removeAllListeners();
+            sut.userCountsByStreamKey = buildInitialUserCountMap();
+            sut.messagesByStreamKey = buildInitialMessageMap();
+        });
+    });
+});
--- backend/chatService.ts
+++ backend/chatService.ts
@@ -8,81 +8,83 @@ import { lobbyConfig, chatConfig } from "./config/configurationConstants.js";
 
 export class ChatService {
   constructor(
-    private io : Server, 
-    private userCountsByStreamKey:Map<string, number>,
-    private messagesByStreamKey:Map<string, ChatData[]>,
+    private io: Server,
+    public userCountsByStreamKey: Map<string, number>,
+    public messagesByStreamKey: Map<string, ChatData[]>,
     private metascraper: Metascraper,
-    private got: Got){}
+    private got: Got,
+    private messageDuration: number) { }
 
-    urlRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/g;
+  urlRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/g;
 
-    incrementUserCount(streamKey: string) {
-      let currentCount: number = this.userCountsByStreamKey.get(streamKey) ?? 0;
-      currentCount++;
-      this.userCountsByStreamKey.set(streamKey, currentCount);
-    } 
-    decrementUserCount(streamKey: string) {
-      let currentCount: number = this.userCountsByStreamKey.get(streamKey) ?? 0;
-      if (!currentCount) {
-        return;
-      }
-    
-      currentCount--;
-    
-      this.userCountsByStreamKey.set(streamKey, 0);
-    }
-
-    async handleChatMessage(streamKey: string, message: ChatData) {
-      message.embed = await this.buildEmbed(message);
-      message.html = toHTML(textEmoji(message.message));
-
-      if (!this.messagesByStreamKey.get(streamKey)) {
-        this.messagesByStreamKey.set(streamKey, []);
-      }
-      const newIndex = this.messagesByStreamKey.get(streamKey)!.push(message) - 1;
-      setTimeout(() => this.messagesByStreamKey.get(streamKey)!.splice(newIndex, 1), 5 * 60 * 1000);
-
-      this.io.to(streamKey).emit("chat", message);
-    }
-
-    async buildEmbed(messageData: { message: string, embed?: Metadata }): Promise<Metadata | undefined> {
-      const urlMatches = messageData.message.match(this.urlRegex);
-  
-      if (urlMatches) {
-          try {
-              const url = urlMatches[0];
-              const { body: html } = await this.got(url);
-              return await this.metascraper({ html, url });
-          } catch (error) {
-              console.error(error);
-          }
-      }
-
+  incrementUserCount(streamKey: string) {
+    let currentCount: number = this.userCountsByStreamKey.get(streamKey) ?? 0;
+    currentCount++;
+    this.userCountsByStreamKey.set(streamKey, currentCount);
+    console.log(this.userCountsByStreamKey);
+  }
+  decrementUserCount(streamKey: string) {
+    let currentCount: number = this.userCountsByStreamKey.get(streamKey) ?? 0;
+    if (!currentCount) {
       return;
+    }
+
+    currentCount--;
+
+    this.userCountsByStreamKey.set(streamKey, 0);
   }
 
-    emitUserLeft(streamKey: string) {
-      this.io.to(streamKey).emit("userLeft", this.userCountsByStreamKey.get(streamKey));
-    }
+  async handleChatMessage(streamKey: string, message: ChatData) {
+    message.embed = await this.buildEmbed(message);
+    message.html = toHTML(textEmoji(message.message));
 
-    emitUserTyping(streamKey: string, username: string) {
-      this.io.to(streamKey).emit("typing", username);
+    if (!this.messagesByStreamKey.get(streamKey)) {
+      this.messagesByStreamKey.set(streamKey, []);
     }
+    const newIndex = this.messagesByStreamKey.get(streamKey)!.push(message) - 1;
+    setTimeout(() => this.messagesByStreamKey.get(streamKey)!.splice(newIndex, 1), this.messageDuration);
 
-    emitUserJoined(streamKey: string) {
-      this.io.to(streamKey).emit("userJoined", this.userCountsByStreamKey.get(streamKey));
-    }
+    this.io.to(streamKey).emit("chat", message);
+  }
 
-    getMessages(streamKey: string) {
-      return this.messagesByStreamKey.get(streamKey);
-    }
+  async buildEmbed(messageData: { message: string, embed?: Metadata }): Promise<Metadata | undefined> {
+    const urlMatches = messageData.message.match(this.urlRegex);
 
-    getConfiguration(configurationType: string) {
-      switch(configurationType){
-        case "lobby":
-          return lobbyConfig;
-        case "chat":
-          return chatConfig;
+    if (urlMatches) {
+      try {
+        const url = urlMatches[0];
+        const { body: html } = await this.got(url);
+        return await this.metascraper({ html, url });
+      } catch (error) {
+        console.error(error);
       }
     }
+
+    return;
+  }
+
+  emitUserLeft(streamKey: string) {
+    this.io.to(streamKey).emit("userLeft", this.userCountsByStreamKey.get(streamKey));
+  }
+
+  emitUserTyping(streamKey: string, username: string) {
+    this.io.to(streamKey).emit("typing", username);
+  }
+
+  emitUserJoined(streamKey: string) {
+    this.io.to(streamKey).emit("userJoined", this.userCountsByStreamKey.get(streamKey));
+  }
+
+  getMessages(streamKey: string) {
+    return this.messagesByStreamKey.get(streamKey);
+  }
+
+  getConfiguration(configurationType: string) {
+    switch (configurationType) {
+      case "lobby":
+        return lobbyConfig;
+      case "chat":
+        return chatConfig;
+    }
+  }
 }
